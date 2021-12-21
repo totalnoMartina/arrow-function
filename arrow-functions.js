58e60dfc64b90845ed712f48d976c3cd988556f9
Martina Grganic <totalnobzvz@yahoo.com>
2021-12-21 22:25:38 +0000

    ADvanced JS, arrow-functions introduction

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,45 @@
  */
 
 // Vanilla JavaScript Function
+// function addTwoNumbers(a, b) {
+//     // Code block
+//     return a + b
+// }
+
+// let sum = addTwoNumbers(3, 5)
+// console.log(sum)
 
 // Arrow Function With Parameters
+// const addTwoNumbers = (a, b) => {
+//     // Code block
+//     return a + b
+// }
+
+// let sum = addTwoNumbers(3, 5)
+// console.log(sum)
 
 // Single Line Arrow Function With Parameters
 
+// const addTwoNumbers = (a, b) => (a + b);
+const addTwoNumbers = (a, b) => a + b;
+
+
+let sum = addTwoNumbers(3, 5)
+console.log(sum)
 // Implicit Returns
+const saySomething = message => console.log(message);
+
+saySomething("Hello there!")
+
+const sayHello = () => console.log('Hello');
+sayHello()
+// Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `    <p>
+    This is a multiline string
+    </p>`
+)
+console.log(returnMultipleLines())
 
 // Returning Multiple Lines
